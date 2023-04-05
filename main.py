2763a3906a3ba9925adce93d3934419a2547f2e5
Furkan Çetin <107766651+frkncetin@users.noreply.github.com>
2023-04-06 01:35:38 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-
-def kredileriListele():
-    krediler = ["Hızlı Kredi", "Maaşını Halkbank'tan alanlara özel" , "Mutlu emekli ihtiyaç kredisi"]
-
-    for kredi in krediler:
-        print("<option>"+kredi+"<option>")
-
-kredileriListele()
-kredileriListele()
